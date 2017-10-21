e14edbbe464dac42d7683b4c09681a5648b692d5
Sawant Shah <sawantshah@gmail.com>
2017-10-22 01:51:36 +0200

    Add JMS Serializer Bundle.

--- .docker/mysql/init.sql
+++ .docker/mysql/init.sql
@@ -8,7 +8,7 @@ CREATE TABLE `inbox` (
   `subject` VARCHAR(255) NOT NULL DEFAULT '',
   `message` TEXT,
   `time_sent` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `is_read` tinyint(1) DEFAULT '0' COMMENT '0 = No, 1 = Yes',
-  `is_archived` tinyint(1) DEFAULT '0' COMMENT '0 = No, 1 = Yes',
+  `is_read` tinyint(1) DEFAULT 0 COMMENT '0 = No, 1 = Yes',
+  `is_archived` tinyint(1) DEFAULT 0 COMMENT '0 = No, 1 = Yes',
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
