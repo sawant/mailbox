d52635e66787facffbbfbbc59269d0896654e52b
Sawant Shah <sawantshah@gmail.com>
2017-10-21 15:59:44 +0200

    Create/load database schema.

---
+++ .docker/mysql/init.sql
@@ -0,0 +1,14 @@
+/*!40101 SET NAMES utf8 */;
+
+DROP TABLE IF EXISTS `inbox`;
+
+CREATE TABLE `inbox` (
+  `uid` INT(11) unsigned NOT NULL AUTO_INCREMENT,
+  `sender` VARCHAR(255) NOT NULL,
+  `subject` VARCHAR(255) NOT NULL DEFAULT '',
+  `message` TEXT,
+  `time_sent` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_read` tinyint(1) DEFAULT '0' COMMENT '0 = No, 1 = Yes',
+  `is_archived` tinyint(1) DEFAULT '0' COMMENT '0 = No, 1 = Yes',
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
