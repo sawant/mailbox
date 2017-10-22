c458f911b40adbee4c144a0f967f80f081884dc9
Sawant Shah <sawantshah@gmail.com>
2017-10-22 03:21:59 +0200

    Test to make sure we are getting the correct response structure.

---
+++ tests/AppBundle/Controller/MailboxControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MailboxControllerTest extends WebTestCase
+{
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->client = static::createClient();
+    }
+
+    /**
+     * @test
+     */
+    public function testListMessages()
+    {
+        $this->client->request('GET', '/api/mailbox');
+
+        $response = $this->client->getResponse();
+        $data     = json_decode($response->getContent(), true);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
+
+        // Verify that result data has the correct keys
+        $this->assertArrayHasKey('uid', $data[0]);
+        $this->assertArrayHasKey('sender', $data[0]);
+        $this->assertArrayHasKey('subject', $data[0]);
+        $this->assertArrayHasKey('message', $data[0]);
+        $this->assertArrayHasKey('time_sent', $data[0]);
+        $this->assertArrayHasKey('read', $data[0]);
+        $this->assertArrayHasKey('archived', $data[0]);
+    }
+}
